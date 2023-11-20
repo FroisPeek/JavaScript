f50015c497a381fac9e9e0a14db6e3a4bd7f014b
Eduardo Frois <126896990+FroisPeek@users.noreply.github.com>
2023-11-20 18:14:55 -0300

    Add files via upload

---
+++ CursoWeb/Iniciando/desafio area.js
@@ -0,0 +1,11 @@
+// area = pi * raio^2
+
+let raio = 10;
+const pi = 3.14159;
+// podemos usar Math.PI
+
+let area = pi * (raio*raio);
+console.log("Valor da area: "+ area + "m²");
+
+area = Math.PI * (raio*raio);
+console.log("Valor da area: "+ area + "m²");
